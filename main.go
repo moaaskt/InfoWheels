135482fd207622011051496fbd0c8d1ebb60204b
moaaskt <moacirmayer@hotmail.com>
2023-07-30 15:07:10 -0300

    init backend

---
+++ main.go
@@ -0,0 +1,71 @@
+// main.go
+
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var users []User
+var vehicles []Vehicle
+
+func main() {
+	router := mux.NewRouter()
+
+	// Rota para criar um novo usuário
+	router.HandleFunc("/api/users", createUser).Methods("POST")
+
+	// Rota para obter todos os usuários
+	router.HandleFunc("/api/users", getUsers).Methods("GET")
+
+	// Rota para criar um novo veículo
+	router.HandleFunc("/api/vehicles", createVehicle).Methods("POST")
+
+	// Rota para obter todos os veículos
+	router.HandleFunc("/api/vehicles", getVehicles).Methods("GET")
+
+	log.Println("Servidor iniciado na porta 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Simulação de criação do usuário no banco de dados
+	users = append(users, user)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func createVehicle(w http.ResponseWriter, r *http.Request) {
+	var vehicle Vehicle
+	err := json.NewDecoder(r.Body).Decode(&vehicle)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Simulação de criação do veículo no banco de dados
+	vehicles = append(vehicles, vehicle)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(vehicle)
+}
+
+func getVehicles(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(vehicles)
+}
---
+++ models.go
@@ -0,0 +1,18 @@
+// models.go
+
+package main
+
+// User representa os dados de um usuário
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Role     string `json:"role"` // Papel/Função do usuário: "admin", "user" ou "company"
+}
+
+// Vehicle representa os dados de um veículo
+type Vehicle struct {
+	ID       string `json:"id"`
+	Brand    string `json:"brand"`
+	Model    string `json:"model"`
+	OwnerID  string `json:"ownerId"` // ID do usuário proprietário do veículo
+}
